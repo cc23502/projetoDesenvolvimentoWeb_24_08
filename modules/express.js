2904916953aa1e3fdb789083d7453e26708d09b1
ANA CAROLINA CATELAN PUPO <u23502@academico.cotuca.unicamp.br>
2023-08-24 22:53:50 -0300

    ajuste css

--- modules/express.js
+++ modules/express.js
@@ -3,6 +3,8 @@ const aplicacao = express();
 const porta = 3000;
 
 //configurando a aplicação para usar ejs
+const path = require("path");
+aplicacao.use(express.static("arquivos"));
 aplicacao.set("views", "./views");
 aplicacao.set("view engine", "ejs");
 
