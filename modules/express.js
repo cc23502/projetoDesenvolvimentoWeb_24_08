6ef3f3a84240c07eb8b630b80c7787f083596dd8
ANA CAROLINA CATELAN PUPO <u23502@academico.cotuca.unicamp.br>
2023-08-31 22:47:53 -0300

    rotas + /home

--- modules/express.js
+++ modules/express.js
@@ -8,9 +8,19 @@ aplicacao.use(express.static("arquivos"));
 aplicacao.set("views", "./views");
 aplicacao.set("view engine", "ejs");
 
-aplicacao.get("/", (req, res) => {
+aplicacao.get("/calculadora", (req, res) => {
     console.log("Calculadora");
     res.render("calculadora");
+});
+
+aplicacao.get("/home", (req, res) => {
+    console.log("Formulario");
+    res.render("Formulario");
+});
+
+aplicacao.get("/vitrine", (req, res) => {
+    console.log("Vitrine");
+    res.render("Vitrine");
 });
 
 aplicacao.listen(porta, () => console.log("Servidor no ar"));
